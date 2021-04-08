71bc1493e4e2ca91535a992985ad0f77407e9189
Bluelephant <l.delacotte@yahoo.fr>
2021-04-08 16:36:40 +0200

    constellation

--- src/components/MyWork/MyWorkPage.vue
+++ src/components/MyWork/MyWorkPage.vue
@@ -2,13 +2,18 @@
 <template>
   <div class="myWork" v-if="skills">
     <img src="../../assets/planete.png" alt="" class="planete" />
-
+    <svg width="1000" height="800" viewport="0 0 1000 800">
+      <polygon id="front-line" fill="none" stroke="#dddddd" points="" />
+      <polygon id="back-line" fill="none" stroke="#dddddd" points="" />
+      <polygon id="design-line" fill="none" stroke="#dddddd" points="" />
+      <polygon id="dev-line" fill="none" stroke="#dddddd" points="" />
+    </svg>
     <div class="skills">
       <div
-        v-for="(skill, index) in skills"
-        :key="skill + index"
+        v-for="skill in skills"
+        :key="skill.name"
         :id="`img-${skill.name}`"
-        class="skill"
+        :class="`skill skill--${skill.category}`"
         @mouseenter="skill.skillHovered = true"
         @mouseleave="skill.skillHovered = false"
       >
@@ -30,7 +35,7 @@
 </template>
 
 <script>
-import gsap from "gsap";
+import gsap, { TweenMax } from "gsap";
 import StarSky from "../Common/StarSky";
 import SkillsRating from "../Common/SkillsRating";
 
@@ -48,70 +53,107 @@ export default {
           imageSrc: require("../../assets/skills-logo/angular.png"),
           skillRate: "4",
           skillHovered: false,
+          category: "front",
         },
         {
           name: "React",
           imageSrc: require("../../assets/skills-logo/reactJs.png"),
           skillRate: "4",
           skillHovered: false,
+          category: "front",
         },
         {
           name: "VueJS",
           imageSrc: require("../../assets/skills-logo/vueJs.png"),
           skillRate: "3",
           skillHovered: false,
+          category: "front",
         },
         {
           name: "Github",
           imageSrc: require("../../assets/skills-logo/gitHub.png"),
           skillRate: "3",
           skillHovered: false,
+          category: "dev",
         },
         {
           name: "NodeJS",
           imageSrc: require("../../assets/skills-logo/nodeJS.png"),
           skillRate: "3",
           skillHovered: false,
+          category: "back",
         },
         {
           name: "MongoDB",
           imageSrc: require("../../assets/skills-logo/mongoDb.png"),
           skillRate: "2",
           skillHovered: false,
+          category: "back",
         },
         {
           name: "MySQL",
           imageSrc: require("../../assets/skills-logo/mySql.png"),
           skillRate: "1",
           skillHovered: false,
+          category: "back",
         },
         {
           name: "Photoshop",
           imageSrc: require("../../assets/skills-logo/photoShop.png"),
           skillRate: "4",
           skillHovered: false,
+          category: "design",
         },
         {
           name: "Indesign",
           imageSrc: require("../../assets/skills-logo/inDesign.png"),
           skillRate: "3",
           skillHovered: false,
+          category: "design",
         },
       ],
+      frontSkills: null,
+      backSkills: null,
+      devSkills: null,
+      designSkills: null,
+      line: "",
     };
   },
   mounted() {
     this.makeLogoFloat();
+    this.frontSkills = document.querySelectorAll(".skill--front");
+    this.backSkills = document.querySelectorAll(".skill--back");
+    this.devSkills = document.querySelectorAll(".skill--dev");
+    this.designSkills = document.querySelectorAll(".skill--design");
+    this.drawConstellation();
   },
   methods: {
+    drawConstellation() {
+      const skills = [
+        this.frontSkills,
+        this.backSkills,
+        this.devSkills,
+        this.designSkills,
+      ];
+      this.line = "";
+      skills.forEach((typesSkill) => { // faire 1 constellation par type de logo
+        for (let i = 0; i < typesSkill.length; i++) {
+          let a = Number(typesSkill[i].getBoundingClientRect().left) + 3;
+          let b = Number(typesSkill[i].getBoundingClientRect().top) + 3;
+          this.line += a + "," + b + " ";
+        }
+        TweenMax.set(`#${skill.lineId}`, { attr: { points: this.line } });
+      });
+    },
     makeLogoFloat() {
       gsap.timeline().to(".skill", {
-        x: "random(-3, 3)",
-        y: "random(-3, 3)",
+        x: "random(-30, 30)",
+        y: "random(-30, 30)",
         duration: 3,
         ease: "sine.inOut",
         repeat: -1,
         repeatRefresh: true,
+        onUpdate: this.drawConstellation,
       });
     },
   },
@@ -126,6 +168,9 @@ export default {
   justify-content: center;
   align-items: center;
 
+  svg {
+    position: absolute;
+  }
   .starSky {
     z-index: 1;
     opacity: 0.4;
@@ -143,6 +188,7 @@ export default {
 
     .skill {
       margin-right: 25px;
+      top: random($limit: 10);
     }
     img {
       max-height: 70px;
