e39ff79cede672f0cba956510b320adbdd48254b
Lea Delacotte <ldelacotte@smb.sqli.com>
2022-01-19 13:06:50 +0100

    Remove loader + upload lighter images

--- src/main.js
+++ src/main.js
@@ -1,46 +1,42 @@
-import Vue from 'vue';
-import App from './App';
-import AboutPage from './components/About/AboutPage';
-import HomePage from './components/Homepage/Homepage';
-import MySkillsPage from './components/MySkills/MySkillsPage';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import App from "./App";
+import AboutPage from "./components/About/AboutPage";
+import HomePage from "./components/Homepage/Homepage";
+import MySkillsPage from "./components/MySkills/MySkillsPage";
+import VueRouter from "vue-router";
 
-
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
-const EventBus = new Vue();
-export default EventBus;
 
 const routes = [
-  { path: '*', redirect: '/home' },
+  { path: "*", redirect: "/home" },
   {
-    path: '/',
+    path: "/",
     component: App,
-    redirect : '/home',
+    redirect: "/home",
     children: [
       {
-        path: 'about',
-        component: AboutPage
+        path: "about",
+        component: AboutPage,
       },
       {
-        path: 'skills',
-        component: MySkillsPage
+        path: "skills",
+        component: MySkillsPage,
       },
       {
-        path: 'home',
-        component: HomePage
+        path: "home",
+        component: HomePage,
       },
-    ]
-  }
-
-]
+    ],
+  },
+];
 const router = new VueRouter({
   routes, // raccourci pour `routes: routes`
-  mode: 'hash',
-})
+  mode: "hash",
+});
 
 new Vue({
   router,
-  
-  render: h => h(App),
-}).$mount('#app')
+
+  render: (h) => h(App),
+}).$mount("#app");
